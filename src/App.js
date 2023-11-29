9d48a083efd3d43ff6411139e66ddafe7f50c366
Archith <vk.archith@gmail.com>
2023-11-29 22:52:18 +0530

    app and settings changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavigationBar from "./components/NavigationBar/NavigationBar";
+/*import NavigationBar from "./components/NavigationBar/NavigationBar";
 import ProfilePopup from "./components/ProfilePopup/ProfilePopup";
 import HomePage from "./pages/HomePage/HomePage";
 import ProductPage from "./pages/ProductPage/ProductPage";
@@ -8,15 +8,16 @@ import SellerChatButton from "./components/SellerChatButton/SellerChatButton";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ImageCarousel from "./components/ImageCarousel/ImageCarousel";
 import ProductDescription from "./components/ProductDescription/ProductDescription";
-import ck1 from "/Users/arnav/Desktop/bitsbids/bitsbids/src/assets/ck1.jpg";
-import ck2 from "/Users/arnav/Desktop/bitsbids/bitsbids/src/assets/ck2.jpg";
-import ck3 from "/Users/arnav/Desktop/bitsbids/bitsbids/src/assets/ck3.jpeg";
-import ck4 from "/Users/arnav/Desktop/bitsbids/bitsbids/src/assets/ck4.jpg";
-import ck5 from "/Users/arnav/Desktop/bitsbids/bitsbids/src/assets/ck5.jpg";
+import ck1 from "./assets/ck1.jpg";
+import ck2 from "./assets/ck2.jpg";
+import ck3 from "./assets/ck3.jpeg";
+import ck4 from "./assets/ck4.jpg";
+import ck5 from "./assets/ck5.jpg";*/
 import Settings from "./pages/Settings/Settings";
 
 function App() {
-  const userEmail = "f20220063@hyderabad.bits-pilani.ac.in";
+  {
+    /*const userEmail = "f20220063@hyderabad.bits-pilani.ac.in";
   const images = [
     { ck1 },
     { ck2 },
@@ -24,7 +25,8 @@ function App() {
     { ck4 },
     { ck5 },
     // ... up to 8 images
-  ];
+  ];*/
+  }
 
   return (
     <div>
